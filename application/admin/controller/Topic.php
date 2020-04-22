dc5ca416515b14138e404f8d9e1f9aa36b256f8c
love1314-ball <3280976413@qq.com>
2020-04-22 09:07:16 +0800

    题目导入制作1.0

--- application/admin/controller/Topic.php
+++ application/admin/controller/Topic.php
@@ -4,6 +4,7 @@ namespace app\admin\controller;
 
 use app\common\controller\AdminBase;
 use think\Log;
+use think\Request;
 use think\Db;
 //题目管理
 
@@ -82,7 +83,7 @@ class Topic extends AdminBase {
             }
 
             $new_topic['choose_id'] = implode( ',', $choose );
-            $new_topic['topic_name'] = input('topic_name');
+            $new_topic['topic_name'] = input( 'topic_name' );
 
             //选项表的id
             $where['topic_id'] = $id;
@@ -123,7 +124,84 @@ class Topic extends AdminBase {
     }
 
     // 导入文件
+
     public function lead() {
-        return $this->fetch('lead');
+        return $this->fetch( 'lead' );
     }
+
+    public function lead_add()
+ {
+        $file = $this->request->file( 'excel' );
+        $info = $file->move( ROOT_PATH . 'public' . DS . 'Excel' );
+        if ( $info ) {
+            $file_name = ROOT_PATH . 'public' . DS . 'excel' . DS . $info->getsaveName();
+            $extension = strtolower( pathinfo( $file_name, PATHINFO_EXTENSION ) );
+            vendor( 'PHPExcel.PHPExcel' );
+            $objPHPExcel = new \PHPExcel();
+            if ( $extension == 'xls' ) {
+                $objReader = \PHPExcel_IOFactory::createReader( 'Excel5' );
+            }
+            if ( $extension == 'xlsx' ) {
+                $objReader = \PHPExcel_IOFactory::createReader( 'Excel2007' );
+            }
+            $obj_PHPExcel = $objReader->load( $file_name, $encode = 'utf-8' );
+            $excel_array = $obj_PHPExcel->getsheet( 0 )->toArray();
+            array_shift( $excel_array );
+            //删除第一个数组( 标题 );
+            
+            /**
+             * 下标1为题目名字
+             * 下标二为正确答案，
+             * 然后后面全部为选项数据
+             */
+            // for ($i=0; $i < count($excel_array) ; $i++) { 
+            //     dump( $excel_array[$i] );
+            // }
+           
+            foreach ($excel_array as $key => $value) {
+                $excel_array[$key] = array_filter($value);
+                //去除空数组
+            }
+          
+
+            foreach ($excel_array as $ke => $val) {
+                for ($i=0; $i < count($val); $i++) { 
+                    dump($val[$i]);
+                }
+            }
+            exit;
+
+            foreach ( $excel_array as $key => $value ) {
+                $data['add_time'] = time();
+
+
+
+                // $data['utel1'] = $value[0];
+                // $data['uname'] = $value[1];
+                // $data['uremark'] = $value[2];
+                // $data['ucol1'] = $value[3];
+                // $data['ucol2'] = $value[4];
+                // $data['ucol3'] = $value[5];
+                // $data['ucol4'] = $value[6];
+                // $data['ucol5'] = $value[7];
+                // $data['ucol6'] = $value[8];
+                // $data['ucol7'] = $value[9];
+                // $data['ucol8'] = $value[10];
+                // $data['ucol9'] = $value[11];
+                // $data['ucol10'] = $value[12];
+                // $data['upc'] = $pcNum;
+                
+               
+                dump( $data );
+            }
+            if ( $res ) {
+                $this->success( '导入成功!' );
+            }
+        } else {
+            //上传失败获取错误信息
+            $this->error( $file->getError() );
+        }
+
+    }
+
 }
