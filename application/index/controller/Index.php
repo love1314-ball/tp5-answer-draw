bef8e2c11ebf99bc81d0489c0beceebfc4e0d20e
love1314-ball <3280976413@qq.com>
2020-04-23 14:16:24 +0800

    增加答题公能，存在刷新页面问题，正在修复

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,63 +6,67 @@ use app\common\controller\IndexBase;
 use think\Db;
 use think\Request;
 use think\Session;
+
 //首页
 
-class Index extends IndexBase {
+class Index extends IndexBase
+{
 
-    public function index() {
-        $username = session( 'user_name' );
-        if ( !$username ) {
-            $this->error( '请登录账号', 'index/Login/index' );
+    public function index()
+    {
+        $username = session('user_name');
+        if (!$username) {
+            $this->error('请登录账号', 'index/Login/index');
         }
-        session( 'topic_all', null );
+        session('topic_all', null);
         //清空session
-        $activity = Db::name( 'activity' )->select();
-        $this->assign( 'activity', $activity );
-        return $this->fetch( 'index' );
+        $activity = Db::name('activity')->select();
+        $this->assign('activity', $activity);
+        return $this->fetch('index');
 
     }
 
     //点进活动内然后进行答题
 
-    public function answer() {
+    public function answer()
+    {
         $request = Request::instance();
-        $id = input( 'answer' );
-        $activity = Db::name( 'activity' )->where( 'id', $id )->find();
-        $time = date( 'Y-m-d', time() );
-        $begin_time = date( 'Y-m-d', $activity['begin_time'] ) ;
-        $finish_time = date( 'Y-m-d', $activity['finish_time'] ) ;
-        if ( $time < $begin_time ) {
+        $id = input('answer');
+        $activity = Db::name('activity')->where('id', $id)->find();
+        $time = date('Y-m-d', time());
+        $begin_time = date('Y-m-d', $activity['begin_time']);
+        $finish_time = date('Y-m-d', $activity['finish_time']);
+        if ($time < $begin_time) {
             return 0;
             echo '<script>alert("活动暂时没开启");window.history.go(-1); </script>';
             exit;
         }
-        if ( $time > $finish_time ) {
+        if ($time > $finish_time) {
             return 1;
             echo '<script>alert("活动已过期");window.history.go(-1); </script>';
             exit;
         }
-        $all = session( 'topic_all' );
-        if ( !$all ) {
+        $all = session('topic_all');
+        if (!$all) {
             $num = 10;
             //需要随机抽取的数据
-            $list = Db::name( 'topic' )->order( 'id desc' )->limit( 100 )->select();
+            $list = Db::name('topic')->order('id desc')->limit(100)->select();
             //获取数据集 总数100条，可以根据自己的实际情况决定是否需要limit
-            shuffle( $list );
+            shuffle($list);
             //打乱数组
-            $new_ary = array_slice( $list, 0, $num );
+            $new_ary = array_slice($list, 0, $num);
             //抽取数据
-            session( 'topic_all', $new_ary );
-            $all = session( 'topic_all' );
-            unset( $new_ary );
+            session('topic_all', $new_ary);
+            $all = session('topic_all');
+            unset($new_ary);
         } else {
             $all = $all;
         }
         //生成链接
         $url = $request->domain();
         $control = '/index/index/response/';
-        foreach ( $all as $key => $value ) {
-            $all[$key]['url'] = $url.$control.'topic_id/'.$value['id'].'/activity_id/'.$id;
+        foreach ($all as $key => $value) {
+            $all[$key]['url'] = $url . $control . 'topic_id/' . $value['id'] . '/activity_id/' . $id;
             $all[$key]['answer_id'] = $id;
         }
         return $all;
@@ -70,26 +74,29 @@ class Index extends IndexBase {
 
     //回答题啊
 
-    public function response() {
+    public function response()
+    {
 
         // $input = input();
         // dump( $input );
         // exit;
-        return $this->fetch( 'response' );
+        return $this->fetch('response');
     }
 
-     // 显示题的内容（不重复）
+    // 显示题的内容（不重复）
 
-     public function particular()
-     {
-         $choose_id = input('choose_id');
-         $topic_id = input('topic_id');
-         $choose_id = '489,490,491,492,493';
-         $al = explode(",", $al);
-         for ($i=0; $i < count($al); $i++) { 
-             $data[] = Db::name('choose')->where('id',$al[$i])->find();
+    public function particular()
+    {
+        $choose_id = input('choose_id');
+        $topic_id = input('topic_id');
+//        $choose_id = '489,490,491,492,493';
+        $choose_id = explode(",", $choose_id);
+        for ($i = 0; $i < count($choose_id); $i++) {
+            $where['id'] = $choose_id[$i];
+            $where['topic_id'] = $topic_id;
+             $data[] = Db::name('choose')->where($where)->find();
          }
         return json($data);
-     }
+    }
 
 }
