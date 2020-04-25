717ab28128ec6b26fd50731f0147464904e16e55
love1314-ball <3280976413@qq.com>
2020-04-25 13:57:44 +0800

    区间概率有问题，暂时没法解决换办法

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -297,19 +297,29 @@ class Index extends IndexBase {
         $i++ ) {
             if ( $activity_rule[$i]['activity_probability'] == $activity_rule[0]['activity_probability'] ) {
                 $probability[$i] = range( 1, $activity_rule[$i]['activity_probability'] );
+                //初始第一次，概率问题
             } else {
                 $h = $i - 1;
                 $probability[$i] = range( $activity_rule[$h]['activity_probability'], $activity_rule[$h]['activity_probability'] + $activity_rule[$i]['activity_probability'] );
+                //中间对的概率
                 if ( $i == count( $activity_rule )-1 ) {
+                    //当最后一个的概率
                     $h = $i - 1;
-                    $probability[$i] = range( $activity_rule[$h]['activity_probability'], $activity_rule[$h]['activity_probability'] + $activity_rule[$i]['activity_probability']+$activity_rule[0]['activity_probability'] );
+                    $probability[$i] = range( $activity_rule[$h]['activity_probability'] + $activity_rule[0]['activity_probability'], $activity_rule[$h]['activity_probability'] + $activity_rule[$i]['activity_probability'] + $activity_rule[0]['activity_probability'] );
                 }
             }
-            // $probability = 123;
+            $always = rand( 1, 100 );
+            //设置次数
+            $specific = '';
+            if ( in_array( $always, $probability[$i] ) ) {
+                $specific =  $activity_rule[$i]['reward_name'];
+            }
+            //这里设置三个抽奖内容，不能多不能少
+            dump( $specific );
             dump( $probability[$i] );
             dump( $activity_rule[$i] );
         }
-
+        exit;
         exit;
 
         $a1 = range( 1, 5 );
